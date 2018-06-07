e92b9dbd7cf18287af415c0e4b72e4a12d897aea
Paul Wright <finp@users.noreply.github.com>
2018-06-07 15:06:38 +0100

    WIP: Push and generic changes (#27)
    * push spike
    
    * rename remote dir
    
    * generic partial
    
    * file tidy
    
    * add metrics
    
    * update site.yml

---
+++ bin/build.sh
@@ -0,0 +1,16 @@
+# Build website
+#
+# Run from root level of repo, eg ./bin/build.sh local-site.yml
+
+# get files from other repos using ruby version of asciidoctor
+
+./bin/ant-include.sh
+
+# generate html
+
+antora $1
+
+#fix html
+
+./bin/fixDiv.sh
+
