84fac16380f66c0a982b663c8be7df1b9dd8ef43
Pawel Paszki <pawelpaszki@gmail.com>
2018-07-05 16:30:48 +0100

    updated the 'bin/build.sh' script to replace sitemap.xml by a template (#198)
    * updated the script to replace sitemap.xml by a template
    
    * change the order so the timestamp is not updated in static file

--- bin/build.sh
+++ bin/build.sh
@@ -14,3 +14,11 @@ antora --clean --pull site.yml
 # fix html
 
 ./bin/fix-div.sh
+
+# replace the generated sitemap.xml by a static one (with updated timestamps)
+
+cp bin/sitemap.xml ./build/site/sitemap.xml
+
+# replace the timestamp from the template by one representing current time and date
+
+sed -i "s/\(<lastmod.*>\)[^<>]*\(<\/lastmod.*\)/\1$(date +"%Y-%m-%dT%H:%M:%S.000Z")\2/" ./build/site/sitemap.xml
