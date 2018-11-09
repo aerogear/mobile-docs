bab88caa7be4672e9d6f5a6d187326d8dbcf583c
jstaffor <jstaffor@redhat.com>
2018-11-09 14:51:56 +0000

    Nav.adoc tested using asciidocter

---
+++ scripts/script.sh
@@ -0,0 +1,51 @@
+read -p "getting-started.adoc"
+asciidoctor ../modules/ROOT/pages/getting-started.adoc
+read -p "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+read -p "mobile-clients.adoc"
+asciidoctor ../modules/ROOT/pages/mobile-clients.adoc
+read -p "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+read -p "con_services.adoc"
+asciidoctor ../modules/ROOT/pages/con_services.adoc
+read -p "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+read -p "identity-management.adoc"
+asciidoctor ../modules/ROOT/pages/identity-management.adoc
+read -p "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+read -p "push-notifications.adoc"
+asciidoctor ../modules/ROOT/pages/push-notifications.adoc
+read -p "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+read -p "mobile-metrics.adoc"
+asciidoctor ../modules/ROOT/pages/mobile-metrics.adoc
+read -p "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+read -p "mobile-cicd.adoc"
+asciidoctor ../modules/ROOT/pages/mobile-cicd.adoc
+read -p "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+read -p "data-sync.adoc"
+asciidoctor ../modules/ROOT/pages/data-sync.adoc
+read -p "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+read -p "device-security.adoc"
+asciidoctor ../modules/ROOT/pages/device-security.adoc
+read -p "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+read -p "showcase-apps.adoc"
+asciidoctor ../modules/ROOT/pages/showcase-apps.adoc
+read -p "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+read -p "ref_api.adoc"
+asciidoctor ../modules/ROOT/pages/ref_api.adoc
+read -p "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+read -p "mobile-cli.adoc"
+asciidoctor ../modules/ROOT/pages/mobile-cli.adoc
+read -p "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+
+read -p "glossary.adoc"
+asciidoctor ../modules/ROOT/pages/glossary.adoc
+read -p "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
