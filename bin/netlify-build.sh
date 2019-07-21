16f03acaf540337b738cf23028e4694130fab5d9
Paul Wright <finp@users.noreply.github.com>
2019-07-21 14:45:18 +0100

    Script netlify (#12)
    * test netlify
    
    * install asciidoctor

--- bin/netlify-build.sh
+++ bin/netlify-build.sh
@@ -1,3 +1,8 @@
 
 echo 'netlify-publish-adoc'
 node_modules/.bin/antora --html-url-extension-style=indexify --pull ../antora-site.yml
+
+# ../bin/publish-adoc.sh > build/site/ds-audit.adoc
+
+echo 'node-publish-adoc'
+node ../bin/index.js ../modules/ROOT/pages/ds-audit.adoc
