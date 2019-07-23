4291d424b13bdc455e8395f483d5bdf66acc45e3
Paul Wright <pwright@redhat.com>
2019-07-23 13:04:31 +0100

    add adoc syntax and script to support publishing downstream

--- bin/netlify-build.sh
+++ bin/netlify-build.sh
@@ -1,3 +1,14 @@
 
 echo 'netlify-publish-adoc'
 node_modules/.bin/antora --html-url-extension-style=indexify --pull ../antora-site.yml
+
+echo 'node-publish-adoc'
+
+gem install asciidoctor
+
+for filename in ../modules/ROOT/pages/*.adoc; do
+
+   echo $filename
+  ../bin/asciidoc-coalescer.rb $filename > ../build/site/$(basename "$filename")
+
+done
