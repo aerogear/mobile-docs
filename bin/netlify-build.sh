4eecbffbd8514e876a2137c5a242ff2586c20286
Paul Wright <pwright@redhat.com>
2019-07-31 17:55:19 +0100

    antora working, fix adoc

--- bin/netlify-build.sh
+++ bin/netlify-build.sh
@@ -10,6 +10,6 @@ gem install asciidoctor
 for filename in ../modules/ROOT/pages/*.adoc; do
 
    echo $filename
-  ../bin/asciidoc-coalescer.rb -a include-tags='!excludeDownstream;!excludeAll' $filename > ../build/site/$(basename "$filename")
+  ../bin/asciidoc-coalescer.rb -a include-tags="!excludeDownstream;!excludeAll" $filename > ../build/site/$(basename "$filename")
 
 done
