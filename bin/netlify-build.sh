62b3cf4868f0883102e8fae198e363e2b206cf0a
Paul Wright <pwright@redhat.com>
2019-08-08 16:24:59 +0100

    add html output

--- bin/netlify-build.sh
+++ bin/netlify-build.sh
@@ -12,6 +12,7 @@ for filename in ../modules/ROOT/pages/*.adoc; do
   echo $filename
   cat ../modules/ROOT/pages/_partials/attributes.adoc ../bin/downstream-attributes $filename > $filename.tmp
   ../bin/asciidoc-coalescer.rb -a include-tags="!excludeDownstream;!excludeAll" $filename.tmp > ../build/site/$(basename "$filename")
+  asciidoctor -a include-tags="!excludeDownstream;!excludeAll" $filename.tmp > ../build/site/$(basename "$filename").html
 
 done
 
