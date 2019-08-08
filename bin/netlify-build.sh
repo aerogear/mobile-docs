82fd151459dbc9c4b47cbb33d0371911bc6c2fa6
Paul Wright <pwright@redhat.com>
2019-08-08 15:28:34 +0100

    fix netlify build

--- bin/netlify-build.sh
+++ bin/netlify-build.sh
@@ -10,8 +10,8 @@ gem install asciidoctor
 for filename in ../modules/ROOT/pages/*.adoc; do
 
   echo $filename
-  cat ../bin/downstream-attributes $filename > temp.txt
-  ../bin/asciidoc-coalescer.rb -a include-tags="!excludeDownstream;!excludeAll" temp.txt > ../build/site/$(basename "$filename")
+  cat ../bin/downstream-attributes $filename > $filename.tmp
+  ../bin/asciidoc-coalescer.rb -a include-tags="!excludeDownstream;!excludeAll" $filename.tmp > ../build/site/$(basename "$filename")
 
 done
 
