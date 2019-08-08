78f9fee59e581b083bd2bd697a16558af5bd7718
Paul Wright <pwright@redhat.com>
2019-08-08 15:35:20 +0100

    add upstream attributes

--- bin/netlify-build.sh
+++ bin/netlify-build.sh
@@ -10,7 +10,7 @@ gem install asciidoctor
 for filename in ../modules/ROOT/pages/*.adoc; do
 
   echo $filename
-  cat ../bin/downstream-attributes $filename > $filename.tmp
+  cat ../modules/ROOT/pages/_partials/attributes.adoc ../bin/downstream-attributes $filename > $filename.tmp
   ../bin/asciidoc-coalescer.rb -a include-tags="!excludeDownstream;!excludeAll" $filename.tmp > ../build/site/$(basename "$filename")
 
 done
