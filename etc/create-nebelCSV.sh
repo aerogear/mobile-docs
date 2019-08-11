d1fcfb600a48d6a7ae802517e20556d8b7f62780
Paul Wright <pwright@redhat.com>
2019-08-11 14:13:56 +0100

    added other docs

--- etc/create-nebelCSV.sh
+++ etc/create-nebelCSV.sh
@@ -6,8 +6,24 @@ for filename in ./*.csv; do
 
  sed -E 's/("([^"]*)")?,/\2\t/g' $filename > $filename.tsv
 
- q -H -t -O -D , "SELECT Category,UserStory,Type,ModuleID,Title,VerifiedInVersion,QuickstartID,Comments,Jira
-FROM ./$filename.tsv WHERE Category = '.'" > nebel$filename
-
 done
+
+
+q -H -t -O -D , "SELECT Category,UserStory,Type,ModuleID,Title,VerifiedInVersion,QuickstartID,Comments,Jira
+FROM ./temp-getting-started.csv.tsv WHERE Category = '.'" > nebel-getting-started.csv
+
+q -H -t -O -D , "SELECT Category,UserStory,Type,ModuleID,Title,VerifiedInVersion,QuickstartID,Comments,Jira
+FROM ./temp-data-sync.csv.tsv WHERE Category = '.'" > nebel-data-sync.csv
+
+q -H -t -O -D , "SELECT Category,UserStory,Type,ModuleID,Title,VerifiedInVersion,QuickstartID,Comments,Jira
+FROM ./temp-push.csv.tsv WHERE Category = '.'" > nebel-push.csv
+
+q -H -t -O -D , "SELECT Category,UserStory,Type,ModuleID,Title,VerifiedInVersion,QuickstartID,Comments,Jira
+FROM ./temp-idm.csv.tsv WHERE Category = '.'" > nebel-idm.csv
+
+q -H -t -O -D , "SELECT Category,UserStory,Type,ModuleID,Title,VerifiedInVersion,QuickstartID,Comments,Jira
+FROM ./temp-security.csv.tsv WHERE Category = '.'" > nebel-security.csv
+
+q -H -t -O -D , "SELECT Category,UserStory,Type,ModuleID,Title,VerifiedInVersion,QuickstartID,Comments,Jira
+FROM ./temp-showcase.csv.tsv WHERE Category = '.'" > nebel-showcase.csv
 
